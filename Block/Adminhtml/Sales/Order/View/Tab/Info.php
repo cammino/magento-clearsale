931b238f6e22a14b81dcb2a40e090c3d558d5390
Marcelo Ferracioli <marcelo@cammino.com.br>
2014-03-07 11:32:17 -0300

    use parent method

--- Block/Adminhtml/Sales/Order/View/Tab/Info.php
+++ Block/Adminhtml/Sales/Order/View/Tab/Info.php
@@ -26,7 +26,7 @@ class Cammino_Clearsale_Block_Adminhtml_Sales_Order_View_Tab_Info extends Mage_A
 		$clearsale = Mage::getModel('cammino_clearsale/gateway');	
 		$url = $clearsale->getScoreUrl($order);
 
-		$html = $this->getChildHtml('order_payment');
+		$html = parent::getPaymentHtml();
 		$html .= "<div style=\"margin-left:-8px;\"><iframe style=\"width:277px;height:96px;border:none;\" src=\"". $url ."\"></iframe></div>";
 
 		return $html;
