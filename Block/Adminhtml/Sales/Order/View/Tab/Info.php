5dc196ba0a1b611314be4747c620e0c8bf27bdf6
Marcelo Ferracioli <marcelo@cammino.com.br>
2015-03-13 16:39:57 -0300

    nao exibe score se o pedido estiver cancelado

--- Block/Adminhtml/Sales/Order/View/Tab/Info.php
+++ Block/Adminhtml/Sales/Order/View/Tab/Info.php
@@ -23,11 +23,13 @@ class Cammino_Clearsale_Block_Adminhtml_Sales_Order_View_Tab_Info extends Mage_A
 	{
 		$order = $this->getOrder();
 
-		$clearsale = Mage::getModel('cammino_clearsale/gateway');	
-		$url = $clearsale->getScoreUrl($order);
-
 		$html = parent::getPaymentHtml();
-		$html .= "<div style=\"margin-left:-8px;\"><iframe style=\"width:277px;height:96px;border:none;\" src=\"". $url ."\"></iframe></div>";
+
+		if ($order->getState() != "canceled") {
+			$clearsale = Mage::getModel('cammino_clearsale/gateway');	
+			$url = $clearsale->getScoreUrl($order);
+			$html .= "<div style=\"margin-left:-8px;\"><iframe style=\"width:277px;height:96px;border:none;\" src=\"". $url ."\"></iframe></div>";
+		}
 
 		return $html;
 	}
