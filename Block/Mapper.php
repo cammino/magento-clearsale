3c35efd089ead5bcbb67cb99a369fac8ba99b010
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2016-06-29 13:07:28 -0300

    Ajusta if que faltou no ultimo commit

--- Block/Mapper.php
+++ Block/Mapper.php
@@ -13,17 +13,19 @@ class Cammino_Clearsale_Block_Mapper extends Mage_Core_Block_Template {
 			$clearSaleSessionId = $sessionId;
 		}
 		
-		$fingeprint = Mage::getStoreConfig("payment_services/clearsale_standard/app_fingeprint");
-		$script = 	"<script>
-					(function (a, b, c, d, e, f, g) {
-					a['CsdpObject'] = e; a[e] = a[e] || function () {
-					(a[e].q = a[e].q || []).push(arguments)
-					}, a[e].l = 1 * new Date(); f = b.createElement(c),
-					g = b.getElementsByTagName(c)[0]; f.async = 1; f.src = d; g.parentNode.insertBefore(f, g)
-					})(window, document, 'script', '//device.clearsale.com.br/p/fp.js', 'csdp');
-					csdp('app', '". $fingeprint ."');
-					csdp('sessionid', '".$clearSaleSessionId."');
-					</script>";
+		if (strpos($path, 'checkout') !== false) {
+			$fingeprint = Mage::getStoreConfig("payment_services/clearsale_standard/app_fingeprint");
+			$script = 	"<script>
+						(function (a, b, c, d, e, f, g) {
+						a['CsdpObject'] = e; a[e] = a[e] || function () {
+						(a[e].q = a[e].q || []).push(arguments)
+						}, a[e].l = 1 * new Date(); f = b.createElement(c),
+						g = b.getElementsByTagName(c)[0]; f.async = 1; f.src = d; g.parentNode.insertBefore(f, g)
+						})(window, document, 'script', '//device.clearsale.com.br/p/fp.js', 'csdp');
+						csdp('app', '". $fingeprint ."');
+						csdp('sessionid', '".$clearSaleSessionId."');
+						</script>";
+		}				
 
 		return $script;
 	}
